c659e8432620e121f9b0632d66e7c9ea50a9140a
MunkyuShin <munkyu.s.shin@gmail.com>
2016-10-19 21:40:24 +0900

    Login

--- app/src/main/java/com/example/firstapplication/MainActivity.kt
+++ app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -2,26 +2,48 @@ package com.example.firstapplication
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
+/**
+ * 1. Create a map saved user id and password.
+ * 2. When user click the login button,
+ *    get id and password that entered.
+ * 3. Compare entered data to saved data.
+ * 4. If they are same, Show "Success Login" message,
+ * 5. else Show "Try again" message
+ */
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val myName: String = "munkyu"
-        printLengthOf(myName)
+        addLoginButtonClickListener()
     }
 
-    fun printLengthOf(name: String) {
-        val length = name.length
-        setText(length.toString())
+    private fun addLoginButtonClickListener() {
+        val loginButton = findViewById(R.id.login_button) as Button
+        loginButton.setOnClickListener { onClickLoginButton() }
     }
 
-    fun setText(text: String) {
-        val textView = findViewById(R.id.text_view) as TextView
-        textView.text = text // Is same with using setText() method.
+    private fun onClickLoginButton() {
+        val idEdit = findViewById(R.id.id_edit_text) as EditText
+        val passwordEdit = findViewById(R.id.password_edit_text) as EditText
+        val id = idEdit.text.toString() // Munkyu
+        val password = passwordEdit.text.toString() //12345
+        login(id, password)
     }
 
+    private fun login(id: String, password: String) {
+        val immutableMap = mapOf<String, Int>(Pair("munkyu", 12345), Pair("lucy", 1234))
+
+        // We are checking data in Map. If you saved data in network then you request login api.
+        if (immutableMap.get(id).toString().equals(password)) {
+            Toast.makeText(applicationContext, "Success Login", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(applicationContext, "Try again", Toast.LENGTH_SHORT).show()
+        }
+    }
 }
