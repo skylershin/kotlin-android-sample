e790c22913aba655212df6a519c15f1b5843140c
MunkyuShin <munkyu.s.shin@gmail.com>
2016-10-28 20:56:17 +0900

    Calculate Karvonen heart rate

--- app/src/main/java/com/example/firstapplication/MainActivity.kt
+++ app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -1,18 +1,11 @@
 package com.example.firstapplication
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.Toast
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 
 /**
- * 1. Create a map saved user id and password.
- * 2. When user click the login button,
- *    get id and password that entered.
- * 3. Compare entered data to saved data.
- * 4. If they are same, Show "Success Login" message,
- * 5. else Show "Try again" message
+ * Calculate Karvonen heart rate. and then print them on TextView.
  */
 class MainActivity : AppCompatActivity() {
 
@@ -20,45 +13,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        addLoginButtonClickListener()
-    }
-
-    private fun addLoginButtonClickListener() {
-        val loginButton = findViewById(R.id.login_button) as Button
-        loginButton.setOnClickListener { onClickLoginButton() }
-    }
-
-    private fun onClickLoginButton() {
-        // Get a EditText instance.
-        val idEdit = findViewById(R.id.id_edit_text) as EditText
-        val passwordEdit = findViewById(R.id.password_edit_text) as EditText
-        // Get a text inside the editTextView
-        val id = idEdit.text.toString() // Munkyu
-        val password = passwordEdit.text.toString() //12345
-        // And Login!! 
-        login(id, password)
-    }
-
-    private fun login(id: String, password: String) {
-        // If you want to any person can't change data, create a immutable map.
-        // In kotlin, provide a immutable map as default option.
-        // If you want to change data later, create mutable map.
-        // val mutableMap = mutableMapOf<String, Int>(Pair("munkyu", 12345), Pair("lucy", 1234))
-        val immutableMap = mapOf<String, Int>(Pair("munkyu", 12345), Pair("lucy", 1234))
-        // Inside a Map
-        //
-        //    key     value
-        // ------------------
-        // | munkyu | 12345 |
-        // ------------------
-        // | lucy   | 1234  |
-        // ------------------
-
-        // We are checking data in Map. If you saved data in network then you request login api.
-        if (immutableMap.get(id).toString().equals(password)) {
-            Toast.makeText(applicationContext, "Success Login", Toast.LENGTH_SHORT).show()
-        } else {
-            Toast.makeText(applicationContext, "Try again", Toast.LENGTH_SHORT).show()
+        val resultTextView = findViewById(R.id.resultTextView) as TextView
+        var result = ""
+        for (intensity in 55..95 step 5) {
+            val bpm = calculateKarvonen(intensity)
+            result += "\n bpm : $bpm"
         }
+        resultTextView.text = result
+    }
+
+    fun calculateKarvonen(intensity: Int): Int {
+        return ((220 - 27) - (100 * intensity / 100)) + 100
     }
 }
