6a519c64ea32d83bf78ae7dbc7aa5b8860f5e07b
Munkyu <munkyu.s.shin@gmail.com>
2017-03-31 20:39:18 +0900

    기본적인 파일 스트럭쳐 구성

---
+++ app/src/main/java/com/example/firstapplication/features/addtask/AddTaskActivity.kt
@@ -0,0 +1,14 @@
+package com.example.firstapplication.features.addtask
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+import com.example.firstapplication.R
+
+class AddTaskActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_task)
+    }
+}
---
+++ app/src/main/java/com/example/firstapplication/features/tasks/TasksActivity.kt
@@ -0,0 +1,13 @@
+package com.example.firstapplication.features.tasks
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.firstapplication.R
+
+class TasksActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tasks)
+
+    }
+}
