d029f1c5ecd594ba86a1c5224ec261bfe4087e54
Munkyu <munkyu.s.shin@gmail.com>
2017-04-01 17:55:20 +0900

    create shared preference module

---
+++ app/src/main/java/com/example/firstapplication/Constant.kt
@@ -0,0 +1,8 @@
+package com.example.firstapplication
+
+/**
+ * Created by munkyushin on 4/1/17.
+ */
+object PreferenceKey {
+
+}
--- app/src/main/java/com/example/firstapplication/MainActivity.kt
+++ app/src/main/java/com/example/firstapplication/MainActivity.kt
@@ -2,34 +2,16 @@ package com.example.firstapplication
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import com.example.firstapplication.StringUtil.combineText
 
 /**
- * Simple Search application
+ * Shared Preference
  *
- * Save colleagues name to List,
- * Get keyword using EditText.
- * And Click the search button, Show the search results.
+ * TODO: create examples.
  */
 class MainActivity : AppCompatActivity() {
-    lateinit var mViewModel:SearchResultViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        mViewModel = SearchResultViewModel();
-        val searchButton = findViewById(R.id.search_button) as Button
-
-        searchButton.setOnClickListener {
-            val searchKeywordEdit = findViewById(R.id.search_keyword_edit) as EditText
-            val searchResultView = findViewById(R.id.search_result_view) as TextView
-            val keyword = searchKeywordEdit.text.toString()
-
-            searchResultView.text = combineText(mViewModel.filter(keyword))
-        }
     }
 }
